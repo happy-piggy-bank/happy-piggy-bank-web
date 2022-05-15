dfec2023616645c2c9130722fd408b2f666fed67
Gary Lee <dokdo2005@gmail.com>
2022-05-16 01:11:34 +0900

    feat: 삼성 인터넷 안내 팝업 추가

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,17 @@ import store from "./features/store";
 import App from "./App";
 import "./css/index.css";
 
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+if (navigator.userAgent.match(/samsung/i)) {
+  alert(
+    "현재 웹표준을 정상적으로 지원하지 않는 브라우저(Samsung Internet)를 사용하고 있습니다. " +
+      "따라서 웹페이지가 의도된 대로 표시되지 않을 수 있으며 " +
+      "웹표준을 정상적으로 지원하는 다른 웹브라우저(Firefox, Microsoft Edge, Google Chrome 등)를 " +
+      "사용하시길 권장합니다."
+  );
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
